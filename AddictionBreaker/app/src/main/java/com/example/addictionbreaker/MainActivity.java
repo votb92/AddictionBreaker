362faca91be5351c03b0b7ec769437e278f8a9ad
Jeffrey <duahjeffrey08@gmail.com>
2020-09-02 23:23:11 -0400

    Add start button

--- AddictionBreaker/app/src/main/java/com/example/addictionbreaker/MainActivity.java
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.addictionbreaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainPage.class);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 }
