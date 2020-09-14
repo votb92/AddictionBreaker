533b954602f8a4094a3c3e03647d3c5973226b2e
Jeffrey <duahjeffrey08@gmail.com>
2020-09-14 14:01:15 -0400

    Alert for missing user info

--- AddictionBreaker/app/src/main/java/com/example/addictionbreaker/Form.java
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/Form.java
@@ -1,11 +1,13 @@
 package com.example.addictionbreaker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Form extends AppCompatActivity {
 
@@ -14,15 +16,28 @@ public class Form extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
-        Button startButton = findViewById(R.id.confirm_button);
+        final EditText name = findViewById(R.id.userName);
+        final EditText age = findViewById(R.id.age);
+
+        final Button startButton = findViewById(R.id.confirm_button);
         startButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Form.this, ConsumptionInfo.class);
-                startActivity(intent);
+                if(name.getText().toString().isEmpty() || age.getText().toString().isEmpty()){
+                    AlertDialog a = new AlertDialog.Builder(startButton.getContext()).create();
+                    a.setTitle("Missing Fields");
+                    a.setMessage("Please enter your name and/or age");
+                    a.show();
+                }
+                else {
+                    Intent intent = new Intent(Form.this, ConsumptionInfo.class);
+                    startActivity(intent);
+                }
         }
 
         });
+
+
     }
 }
