5cc84e7f5eaa5a73295350a11366bab28031ef7a
Jeffrey <duahjeffrey08@gmail.com>
2020-09-18 23:28:24 -0400

    Save user info to sharedpreferences

--- AddictionBreaker/app/src/main/java/com/example/addictionbreaker/Form.java
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/Form.java
@@ -3,12 +3,18 @@ package com.example.addictionbreaker;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.google.gson.Gson;
 
 public class Form extends AppCompatActivity {
 
@@ -22,10 +28,14 @@ public class Form extends AppCompatActivity {
         final Spinner addiction = findViewById(R.id.addictionSpinner);
 
         final Button startButton = findViewById(R.id.confirm_button);
+        final Gson gson = new Gson();
+        final SharedPreferences myPrefs = this.getSharedPreferences("com.example.app", Context.MODE_PRIVATE);
+
         startButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
+                //Log.i("test", name.getText().toString());
                 if(name.getText().toString().isEmpty() || age.getText().toString().isEmpty()){
                     AlertDialog a = new AlertDialog.Builder(startButton.getContext()).create();
                     a.setTitle("Missing Fields");
@@ -34,6 +44,9 @@ public class Form extends AppCompatActivity {
                 }
                 else {
                     User user = new User(name.getText().toString(),addiction.getSelectedItem().toString(),Integer.parseInt(age.getText().toString()));
+                    String info = gson.toJson(user);
+                    myPrefs.edit().putString("userInfo", info).commit();
+
                     Intent intent = new Intent(Form.this, ConsumptionInfo.class);
                     startActivity(intent);
                 }
