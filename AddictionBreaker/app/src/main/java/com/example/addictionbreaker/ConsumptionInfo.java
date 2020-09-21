8aefae0f470bfe235d70afda1206ac1d71fcb21d
votb92 <votb@mymail.vcu.edu>
2020-09-21 03:43:33 -0400

    update string placeholders to match choices

--- AddictionBreaker/app/src/main/java/com/example/addictionbreaker/ConsumptionInfo.java
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/ConsumptionInfo.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,12 +16,16 @@ import android.widget.TextView;
 import com.google.gson.Gson;
 
 public class ConsumptionInfo extends AppCompatActivity {
-
+    private String string1;
+    private String string2;
+    private TextView consumption_info_how_frequent  ;
+    private TextView consumption_info_cost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumption_info);
-
+        consumption_info_how_frequent = (TextView)findViewById(R.id.consumption_info_how_frequent);
+        consumption_info_cost = (TextView)findViewById(R.id.consumption_info_cost);
         final EditText averageConsumption = findViewById(R.id.averageConsumption);
         final EditText averageCost = findViewById(R.id.averageCost);
         Button letsGo = findViewById(R.id.letsGo);
@@ -30,6 +35,14 @@ public class ConsumptionInfo extends AppCompatActivity {
         final Gson gson = new Gson();
         String info = myPrefs.getString("userInfo", null);
         final User user = gson.fromJson(info , User.class);
+        setStrings(user);
+
+        Resources res = getResources();
+        String how_frequent = String.format(res.getString(R.string.consumption_info_how_frequent), string1);
+        String cost = String.format(res.getString(R.string.consumption_info_cost),string2);
+        consumption_info_how_frequent.setText(how_frequent);
+        consumption_info_cost.setText(cost);
+
 
         letsGo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +57,23 @@ public class ConsumptionInfo extends AppCompatActivity {
             }
         });
 
-
+    }
+    private void setStrings(User user) {
+        if (user.getAddiction().equals("Cigarettes")){
+            string1 = "cigarettes";
+            string2 = "a pack of cigarettes";
+        }
+        else if(user.getAddiction().equals("Alcohol")){
+            string1 = "drinks";
+            string2 = "a day of drinking";
+        }
+        else if(user.getAddiction().equals("Vaping/Juuling")){
+            string1 = "pod";
+            string2 = "a pod";
+        }
+        else{
+            string1 = "cigarettes";
+            string2 = "a pack of cigarettes";
+        }
     }
 }
