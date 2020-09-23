6eb0373272b5fff006ca8005123d3413bcd2e377
Jeffrey <duahjeffrey08@gmail.com>
2020-09-23 15:36:27 -0400

    DatePicker for sobriety start date

--- AddictionBreaker/app/src/main/java/com/example/addictionbreaker/ui/ConsumptionInfoActivity.java
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/ui/ConsumptionInfoActivity.java
@@ -2,6 +2,7 @@ package com.example.addictionbreaker.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,28 +10,34 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.addictionbreaker.R;
 import com.example.addictionbreaker.model.User;
 import com.google.gson.Gson;
+
+import java.util.Calendar;
 //a comment
 
-public class ConsumptionInfoActivity extends AppCompatActivity {
+public class ConsumptionInfoActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
     private String string1;
     private String string2;
     private TextView consumption_info_how_frequent  ;
     private TextView consumption_info_cost;
+    private TextView startDateText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consumption_info);
         consumption_info_how_frequent = (TextView)findViewById(R.id.consumption_info_how_frequent);
         consumption_info_cost = (TextView)findViewById(R.id.consumption_info_cost);
+        startDateText = findViewById(R.id.start_date);
         final EditText averageConsumption = findViewById(R.id.averageConsumption);
         final EditText averageCost = findViewById(R.id.averageCost);
         Button letsGo = findViewById(R.id.letsGo);
+        Button startDate = findViewById(R.id.start_date_button);
 
         //retrieve user info
         final SharedPreferences myPrefs = this.getSharedPreferences("com.example.app", Context.MODE_PRIVATE);
@@ -59,6 +66,13 @@ public class ConsumptionInfoActivity extends AppCompatActivity {
             }
         });
 
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePickerDialog();
+            }
+        });
+
     }
     private void setStrings(User user) {
         if (user.getAddiction().equals("Cigarettes")){
@@ -77,5 +91,20 @@ public class ConsumptionInfoActivity extends AppCompatActivity {
             string1 = "cigarettes";
             string2 = "a pack of cigarettes";
         }
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, this,
+                Calendar.getInstance().get(Calendar.YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        String date = "Start Date: " + month + "/" + day + "/" + year;
+        startDateText.setText(date);
     }
 }
