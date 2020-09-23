d5290112510143a3714f85b218a93ee35c2b50c7
votb92 <votb@mymail.vcu.edu>
2020-09-22 23:35:12 -0400

    Create DatabaseHelper.java

---
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/DatabaseHelper.java
@@ -0,0 +1,23 @@
+package com.example.addictionbreaker;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
