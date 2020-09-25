f60d5eed96dd8c82d5b70d3e08e9db8e940c16d5
DESKTOP-VLL9RC0\jibri <jibrilhart3@gmail.com>
2020-09-25 00:25:48 -0400

    Wrapped up the the addiction list for the most part. Still needs integration to SQL.

--- AddictionBreaker/app/src/main/java/com/example/addictionbreaker/AdictionList.java
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/AdictionList.java
@@ -6,17 +6,23 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.View;
 
 public class AdictionList extends AppCompatActivity {
 
-    String s1[], s2[];
-    int images[] = {R.drawable.add, R.drawable.add2};
+    RecyclerView recyclerView;
+
+    String s1[];
+    int images[] = {R.drawable.add, R.drawable.add2, R.drawable.add3, R.drawable.add4, R.drawable.add5, R.drawable.add6,};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adiction_list);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,8 +35,11 @@ public class AdictionList extends AppCompatActivity {
             }
         });
 
+        s1 = getResources().getStringArray(R.array.addictions);
         recyclerView = findViewById(R.id.recyclerView);
 
-        com.jovanovic.stefan.myrecyclerview.MyAdapter MyAdapter = new MyAdapter(ct: this, s1, s2, images);
+        MyAdapter myAdapter = new MyAdapter(this, s1, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- AddictionBreaker/app/src/main/java/com/example/addictionbreaker/MyAdapter.java
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/MyAdapter.java
@@ -1,44 +1,55 @@
-package com.jovanovic.stefan.myrecyclerview;
+package com.example.addictionbreaker;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyviewHolder> {
 
-    String data1[], data2[];
+    String data1[];
     int images[];
     Context context;
 
-    public MyAdapter(Context ct, String s1[], String s2[], int images[]){
+    public MyAdapter(Context ct, String s1[], int img[]){
         context = ct;
         data1 = s1;
-        data2 = s2;
         images = img;
     }
 
     @NonNull
     @Override
     public MyviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyviewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyviewHolder holder, int position) {
-
+        holder.myText1.setText(data1[position]);
+        holder.myImage.setImageResource(images[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return images.length;
     }
 
     public class MyviewHolder extends RecyclerView.ViewHolder {
 
+        TextView myText1;
+        ImageView myImage;
+
         public MyviewHolder(@NonNull View itemView) {
             super(itemView);
+            myText1 = itemView.findViewById(R.id.addiction_txt);
+            myImage = itemView.findViewById(R.id.ADDimageView);
         }
     }
 }
