937a62e31a1b0e0c83b49b30096171386afe4d5e
Kyle Vo <54372965+votb92@users.noreply.github.com>
2020-11-12 18:50:19 -0500

    Quick fix (#74)
    * creating unit test
    
    * bug fixing and add framework for journal tab
    
    * journey fragment completed
    
    * fixing some bugs
    
    * framework to dynamically changing drawables
    
    * fixed it

--- AddictionBreaker/app/src/main/java/com/example/addictionbreaker/model/Page.java
+++ AddictionBreaker/app/src/main/java/com/example/addictionbreaker/model/Page.java
@@ -1,62 +1,62 @@
-package com.example.interactivestory.model;
-
-public class Page {
-    private int imageId;
-    private int textId;
-    private Choice choice1;
-    private Choice choice2;
-    private boolean isFinalPage = false;
-
-    public Page(int imageId, int textId, Choice choice1, Choice choice2) {
-        this.imageId = imageId;
-        this.textId = textId;
-        this.choice1 = choice1;
-        this.choice2 = choice2;
-    }
-
-    public Page(int imageId, int textId) {
-        this.imageId = imageId;
-        this.textId = textId;
-        this.isFinalPage = true;
-    }
-
-    public boolean isFinalPage() {
-        return isFinalPage;
-    }
-
-    public void setFinalPage(boolean finalPage) {
-        isFinalPage = finalPage;
-    }
-
-    public int getImageId() {
-        return imageId;
-    }
-
-    public void setImageId(int imageId) {
-        this.imageId = imageId;
-    }
-
-    public int getTextId() {
-        return textId;
-    }
-
-    public void setTextId(int textId) {
-        this.textId = textId;
-    }
-
-    public Choice getChoice1() {
-        return choice1;
-    }
-
-    public void setChoice1(Choice choice1) {
-        this.choice1 = choice1;
-    }
-
-    public Choice getChoice2() {
-        return choice2;
-    }
-
-    public void setChoice2(Choice choice2) {
-        this.choice2 = choice2;
-    }
-}
+//package com.example.interactivestory.model;
+//
+//public class Page {
+//    private int imageId;
+//    private int textId;
+//    private Choice choice1;
+//    private Choice choice2;
+//    private boolean isFinalPage = false;
+//
+//    public Page(int imageId, int textId, Choice choice1, Choice choice2) {
+//        this.imageId = imageId;
+//        this.textId = textId;
+//        this.choice1 = choice1;
+//        this.choice2 = choice2;
+//    }
+//
+//    public Page(int imageId, int textId) {
+//        this.imageId = imageId;
+//        this.textId = textId;
+//        this.isFinalPage = true;
+//    }
+//
+//    public boolean isFinalPage() {
+//        return isFinalPage;
+//    }
+//
+//    public void setFinalPage(boolean finalPage) {
+//        isFinalPage = finalPage;
+//    }
+//
+//    public int getImageId() {
+//        return imageId;
+//    }
+//
+//    public void setImageId(int imageId) {
+//        this.imageId = imageId;
+//    }
+//
+//    public int getTextId() {
+//        return textId;
+//    }
+//
+//    public void setTextId(int textId) {
+//        this.textId = textId;
+//    }
+//
+//    public Choice getChoice1() {
+//        return choice1;
+//    }
+//
+//    public void setChoice1(Choice choice1) {
+//        this.choice1 = choice1;
+//    }
+//
+//    public Choice getChoice2() {
+//        return choice2;
+//    }
+//
+//    public void setChoice2(Choice choice2) {
+//        this.choice2 = choice2;
+//    }
+//}
